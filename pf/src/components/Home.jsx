a83dacf4300212d3385b3cb7d0418e0c8e1e9f6f
ajeer333 <ajeer333@gmail.com>
2023-04-15 17:48:31 +0200

    commit

--- pf/src/components/Home.jsx
+++ pf/src/components/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 mt-12"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
