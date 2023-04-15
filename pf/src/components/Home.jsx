0d7e0ec1ddf4a197b0364b692ca94b2de76591ec
ajeer333 <ajeer333@gmail.com>
2023-04-15 17:44:36 +0200

    commit

--- pf/src/components/Home.jsx
+++ pf/src/components/Home.jsx
@@ -1,9 +1,62 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 // import { Link } from "react-scroll";
 
 const Home = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/hena-asslam/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/HenaAslam/",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:henaaslam111@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          <FaInstagram size={30} />
+        </>
+      ),
+      href: "https://instagram.com/henomnom?igshid=YjNmNGQ3MDY=",
+    },
+    {
+      id: 5,
+      child: (
+        <>
+          <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/Resume_Asslam..pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
   return (
     <div
       name="home"
@@ -31,6 +84,25 @@ const Home = () => {
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </button>
+          </div>
+          <div className="flex text-white lg:hidden  my-2">
+            {links.map((link) => (
+              <div
+                key={link.id}
+                className="bg-gray-500 mx-1 p-2 cursor-pointer rounded"
+              >
+                <a
+                  href={link.href}
+                  className="flex justify-between items-center w-full text-white"
+                  download={link.download}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  {link.child}
+                </a>
+              </div>
+            ))}
           </div>
         </div>
 
