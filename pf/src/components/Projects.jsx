ed8aab7e66b43521586a4e3228abc9dfbdf66d00
ajeer333 <ajeer333@gmail.com>
2023-04-16 13:41:35 +0200

    commit

--- pf/src/components/Projects.jsx
+++ pf/src/components/Projects.jsx
@@ -6,54 +6,70 @@ import quizapp from "../assets/portfolio/quiz-app.png";
 import spotifyhome from "../assets/portfolio/spotify-home.png";
 import spotify from "../assets/portfolio/spotify.png";
 import weather from "../assets/portfolio/weather-app.png";
-import installNode from "../assets/portfolio/installNode.jpg";
+import portfolio from "../assets/portfolio/portfolio.png";
+import linkedinhome from "../assets/portfolio/linkedinhome.png";
+import "../styles.css";
 const Projects = () => {
   const portfolios = [
     {
       id: 1,
       src: linkedin,
-      technologies: ["react", "redux", "bootstrap"],
+      project: "LinkedIn Frontend",
+      technologies: ["Typescript", "React", "Redux", "Bootstrap", "API"],
     },
     {
       id: 2,
-      src: linkedin,
-      technologies: ["react", "redux", "bootstrap"],
+      src: linkedinhome,
+      project: "LinkedIn Backend",
+      technologies: [" ExpressJS", "MongoDB", "NodeJS", "API"],
     },
     {
       id: 3,
-      src: linkedin,
-      technologies: ["react", "redux", "bootstrap"],
+      src: weather,
+      project: "Weather App",
+      technologies: ["React", "Bootstrap", "Redux", "API"],
     },
     {
       id: 4,
-      src: linkedin,
-      technologies: ["react", "redux", "bootstrap"],
+      src: spotify,
+      project: "Spotify (Javascript)",
+      technologies: ["Javascript", "Bootstrap", "API"],
     },
     {
       id: 5,
-      src: linkedin,
-      technologies: ["react", "redux", "bootstrap"],
+      src: spotifyhome,
+      project: "Spotify (React)",
+      technologies: ["React", "Redux", "API", "Bootstrap"],
     },
     {
       id: 6,
-      src: linkedin,
-      technologies: ["react", "redux", "bootstrap"],
+      src: quizapp,
+      project: "Quiz App",
+      technologies: ["HTML", "CSS", "Javascript"],
     },
     {
       id: 7,
-      src: linkedin,
-      technologies: ["react", "redux", "bootstrap"],
+      src: mediumclone,
+      project: "Medium Clone",
+      technologies: ["HTML", "CSS", "Javascript"],
     },
     {
       id: 8,
-      src: linkedin,
+      src: onlinecourse,
+      project: "Netflix",
       technologies: ["react", "redux", "bootstrap"],
+    },
+    {
+      id: 9,
+      src: portfolio,
+      project: "Portfolio",
+      technologies: ["React", "Tailwind", "Javascript"],
     },
   ];
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen content"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -64,37 +80,25 @@ const Projects = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, technologies }) => (
+          {portfolios.map(({ id, src, technologies, project }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={src}
-                alt=""
-                className="rounded-md duration-200 hover:scale-105"
-              />
+              <div>
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105"
+                />
+                <h6 className="flex justify-center items-center pt-3 pb-2 text-purple-500 text-xl font-bold">
+                  {project}
+                </h6>
+              </div>
 
-              {/* <div className="flex items-center justify-evenly px-1">
+              <div class="grid grid-cols-3 gap-4 font-mono text-white text-sm font-bold leading-6 mb-2 px-2 ">
                 {technologies.map((tech) => (
-                  <div className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-cyan-800 rounded">
+                  <div className=" grid rounded-lg justify-items-center bg-purple-500  ">
                     {tech}
                   </div>
                 ))}
-              </div> */}
-              <div className="grid sm:grid-cols-3 md:grid-cols-3  gap-0 ">
-                <button className=" m-4 duration-200 hover:scale-105 bg-cyan-950 rounded ">
-                  Demo
-                </button>
-                <button className=" m-4 duration-200 hover:scale-105 bg-cyan-950 rounded">
-                  Code
-                </button>
-                <button className=" m-4 duration-200 hover:scale-105 bg-cyan-950 rounded">
-                  Code
-                </button>
-                <button className=" m-4 duration-200 hover:scale-105 bg-cyan-950 rounded ">
-                  Code
-                </button>
-                <button className=" m-4 duration-200 hover:scale-105 bg-cyan-950 rounded">
-                  Code
-                </button>
               </div>
             </div>
           ))}
