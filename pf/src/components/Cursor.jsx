205b0535fcbb279ca92623286943a06c0d9f6fbd
ajeer333 <ajeer333@gmail.com>
2023-04-24 13:06:02 +0200

    Update Cursor.jsx

--- pf/src/components/Cursor.jsx
+++ pf/src/components/Cursor.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "../styles.css";
 function Cursor() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [isTouching, setIsTouching] = useState(false);
 
   useEffect(() => {
     const handleMouseMove = (event) => {
@@ -12,28 +11,20 @@ function Cursor() {
     const handleTouchMove = (event) => {
       setPosition({ x: event.touches[0].clientX, y: event.touches[0].clientY });
     };
-    const handleTouchEnd = () => {
-      setIsTouching(false);
-    };
 
     document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("touchmove", handleTouchMove);
-    document.addEventListener("touchend", handleTouchEnd);
+
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("touchmove", handleTouchMove);
-      document.removeEventListener("touchend", handleTouchEnd);
     };
   }, []);
 
   return (
     <div
       className="custom-cursor"
-      style={{
-        left: isTouching ? position.x : position.x,
-        top: isTouching ? position.y : position.y,
-        backgroundColor: isTouching ? "red" : "white",
-      }}
+      style={{ left: position.x, top: position.y }}
     ></div>
   );
 }
